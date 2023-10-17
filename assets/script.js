6c771d243ac5879313b08028dc03198f06821e4f
Acolyte03 <Acolyte03@gmail.com>
2023-10-17 10:53:51 -0500

    Got values to save to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,50 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(window).on("load", function() {
+  getLocalMeeting();
+});
+$(button).on("click", function(event)
+{
+    event.preventDefault();
+    var meeting = document.querySelector(".description").value;
+
+    setLocalMeeting(meeting);
+});
+
+function setLocalMeeting(valuePassed)
+{
+    var info = localStorage.getItem("meeting");
+
+    var meetingSearch = [];
+    meetingSearch.push(info);
+    localStorage.setItem("meeting", JSON.stringify(meetingSearch));
+    console.log(meetingSearch + " saved.");
+
+    info = valuePassed;
+    localStorage.setItem("meeting", info);
+
+    if (info.indexOf(valuePassed) === -1)
+    {
+        info = info + ',' + valuePassed;
+        localStorage.setItem("meeting", info);
+    }
+}
+
+function getLocalMeeting()
+{ 
+    //var savedFood = JSON.parse(localStorage.getItem("food"));
+    var savedMeeting = localStorage.getItem("meeting");
+    var saveArr = [];
+
+    savedMeeting.trim();
+    saveArr = savedMeeting.split(',');
+
+    for (var i = 0; i < saveArr.length; i++)
+    {
+        appendFoodHistory(saveArr[i]);
+    }
+}
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +63,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
